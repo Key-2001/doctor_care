ada2f3a63691211ec41f13a6f203a8463418a0b4
La Minh Hoạt <lahoat2k1@keylyla.local>
2023-09-15 23:40:50 +0700

    update doctor and about page fe

---
+++ src/pages/Doctors/FeedbackForm.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
+
+const FeedbackForm = () => {
+  //! Props
+
+  //! State
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+  //! Function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  //! Render
+  return (
+    <form>
+      <div>
+        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+          How would you rate the overall experience?*
+        </h3>
+        <div>
+          {[...Array(5).keys()].map((_, index) => {
+            index += 1;
+            return (
+              <button
+                key={index}
+                type="button"
+                onClick={() => setRating(index)}
+                className={`${
+                  index <= ((rating && hover) || hover)
+                    ? "text-yellowColor"
+                    : "text-gray-400"
+                } bg-transparent border-none outline-none text-[22px] cursor-pointer`}
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(rating)}
+                onDoubleClick={() => {
+                  setHover(0);
+                  setRating(0);
+                }}
+              >
+                <span>
+                  <AiFillStar />
+                </span>
+              </button>
+            );
+          })}
+        </div>
+
+        <div className="mt-[30px]">
+          <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0">
+            Share your feedback or suggestions*
+          </h3>
+          <textarea
+            className="border border-solid border-[#0066ff34] focus:outline outline-primaryColor w-full px-4 py-3 rounded-md"
+            rows={5}
+            placeholder="Write your message"
+            value={reviewText}
+            onChange={(e) => setReviewText(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit" className="btn" onClick={handleSubmit}>
+          Submit Feedback
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default FeedbackForm;
